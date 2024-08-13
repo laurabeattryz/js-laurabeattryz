677381924fc7e1c22e61a9dad1a8978780a37b85
laurabeattryz <laurabeadeolis@gmail.com>
2024-08-13 15:36:18 -0300

    adicionado arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+    function setup () {
+        creatCanvas(600, 600);
+        background(“black”);
+    }
+
+        function draw () {
+        strock(“blue“);
+        fill(“red“);
+
+        if(mouseIspressed){
+            rect(mouseX, mouse Y, 20, 35);
+        }
+
+    }
+    
